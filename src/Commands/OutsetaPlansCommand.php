0424aaa6c64162ff78820ec8dacfbf01d1078b08
Jon Fackrell <jon.fackrell@gmail.com>
2025-02-13 18:19:04 -0700

    Added AddOn listing to outseta:plans command

--- src/Commands/OutsetaPlansCommand.php
+++ src/Commands/OutsetaPlansCommand.php
@@ -16,7 +16,7 @@ class OutsetaPlansCommand extends Command
     public function handle(): int
     {
         $response = Http::withToken('Outseta '.config('outseta.api_key').':'.config('outseta.api_secret'))
-            ->get('https://'.config('outseta.api.subdomain').'.outseta.com/api/v1/billing/plans')
+            ->get('https://'.config('outseta.api.subdomain').'.outseta.com/api/v1/billing/plans?fields=Name,Uid,PlanAddOns.*,PlanAddOns.AddOn.Name,PlanAddOns.AddOn.Uid')
             ->json();
 
         $data = collect(data_get($response, 'items'))
@@ -24,12 +24,15 @@ class OutsetaPlansCommand extends Command
                 return [
                     'Name' => data_get($item, 'Name'),
                     'Uid' => data_get($item, 'Uid'),
+                    'Plan Add-ons' => collect(data_get($item, 'PlanAddOns'))->map(function ($item) {
+                        return $item['AddOn']['Name'].': '.$item['AddOn']['Uid'];
+                    })->join("\n"),
                 ];
             })
             ->toArray();
 
         table(
-            headers: ['Plan Name', 'Uid'],
+            headers: ['Plan Name', 'Uid', 'Plan Add-ons'],
             rows: $data
         );
 
