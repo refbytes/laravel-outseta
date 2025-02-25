63ba24373fcb292e205b70d10ec51dff66c197dd
Jon Fackrell <jon.fackrell@gmail.com>
2025-02-25 12:25:54 -0700

    Revert "Added HasAccount to USer stub for phpstan"
    This reverts commit fea76bbc6eb0ac3a06d9ea35924d6edf64a3f14a.

--- phpstan-stubs.php
+++ phpstan-stubs.php
@@ -2,13 +2,9 @@
 
 namespace App\Models;
 
-use RefBytes\Outseta\Models\Traits\HasAccount;
-
 /**
  * @property int $id
  * @property string $name
  * @property string $email
  */
-class User {
-    use HasAccount;
-}
+class User {}
